bea6887ffd9e83f037888901b5403c67456c0844
Mark Estropia <estropia.kio@gmail.com>
2026-06-12 16:51:21 +0800

    Added transition

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import MobileContactBar from './MobileContactBar.jsx'
+import ScrollToTop from './ScrollToTop.jsx'
 
 export default function Layout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <main><Outlet /></main>
       <Footer />
